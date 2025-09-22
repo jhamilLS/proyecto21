06e40493e54cc74b1f24023d6519419e07921af5
jhamilLS <lopezsalgueirojhamil@gmail.com>
2025-09-22 02:02:29 -0400

    Add files via upload
    1

---
+++ resp.js
@@ -0,0 +1,10 @@
+function resp(){
+    var n=document.getElementById("n").value;
+    if (n=="π+1"){
+        alert("me alegra que lo recuerdes tqm y feliz 21");
+    }
+    else{
+        alert("no puedes poner pi pon π");
+        alert("o te olvidaste nuestro codigo :(");
+    }
+}
